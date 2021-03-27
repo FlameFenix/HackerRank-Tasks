4b3b8d264a1df76fe1ae1e573ed59b3a44bfca14
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-03-27 21:49:41 +0200

    Practice

---
+++ BirthdayCakeCandles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BirthdayCakeCandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> candles = new List<int>() { 82, 49, 82, 82, 41, 82 ,15, 63 ,38 ,25 };
+
+            int maxValue = candles.Max();
+            int counter = 0;
+
+            foreach (var item in candles.OrderBy(x => x))
+            {
+                if(item == maxValue)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
