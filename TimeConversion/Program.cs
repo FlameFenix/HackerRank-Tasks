7112aebd3aad712dd58b2f24785a4788b15830cc
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-04-04 17:18:33 +0300

    Exercises

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s = "12:01:00AM";
+
+            DateTime convert = DateTime.Parse(s);
+
+            Console.WriteLine(convert.ToString("HH:mm:ss"));
+        }
+    }
+}
