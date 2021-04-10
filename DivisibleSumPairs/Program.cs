cf9aa628b58ae0957ca3ef7af9badba3c9020ec1
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-04-10 23:33:59 +0300

    Practice

---
+++ DivisibleSumPairs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DivisibleSumPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = new int[] { 1, 3, 2, 6, 1, 2 };
+            int k = 3;
+            int counter = 0;
+
+            for (int x = 0; x < ar.Length; x++)
+            {
+                for (int i = x + 1; i < ar.Length; i++)
+                {
+                    if((ar[x] + ar[i]) % k == 0)
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
