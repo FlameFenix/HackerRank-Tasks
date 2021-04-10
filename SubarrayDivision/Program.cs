683eba16858089fe03b8dce51b2fe87af7130b16
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-04-10 23:20:14 +0300

    Practice

---
+++ SubarrayDivision/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SubarrayDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> s = new List<int>() { 1 ,1 ,1 ,1 ,1 ,1 };
+
+            int d = 3;
+            int m = 2;
+
+            int counter = 0;
+
+            for (int i = 0; i <= s.Count - m; i++)
+            {
+                int sum = 0;
+                for (int y = 0; y < m; y++)
+                {
+                    sum += s[i + y];
+                }
+                if(sum == d)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
