6103ec7b7f1886f5a92c046fcaa80e8254eea27a
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-04-11 23:54:01 +0300

    Practice

---
+++ NumberLineJumps/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NumberLineJumps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 0;
+            int v1 = 2;
+
+            int x2 = 5;
+            int v2 = 3;
+            bool isFound = false;
+            while (x1 != x2)
+            {
+                x1 += v1;
+                x2 += v2;
+                if(x1 == x2)
+                {
+                    Console.WriteLine("YES");
+                    isFound = true;
+                    break;
+                }
+                if(x1 > x2 && v1 > v2)
+                {
+                    break;
+                }
+                else if (x2 > x1 && v2 > v1)
+                {
+                    break;
+                }
+            }
+
+            if(!isFound)
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
