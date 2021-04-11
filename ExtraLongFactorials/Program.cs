0f6090a697a5cd293c398acc4a2e4d4ce9e12518
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-04-11 17:29:59 +0300

    Practice

---
+++ ExtraLongFactorials/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace ExtraLongFactorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger factorial = new BigInteger();
+            factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
