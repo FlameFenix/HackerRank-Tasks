27ba09f7c4be04da4148c1092325779157aab002
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-04-13 20:56:53 +0300

    Practice

---
+++ ElectronicsShop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElectronicsShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 10;
+            int[] keyboards = new int[] { 3, 1 };
+
+            List<int> inputKeyboards = new List<int>(keyboards);
+
+            int[] drives = new int[] { 5, 2, 8 };
+
+            int expensiveThings = -1;
+
+            while (inputKeyboards.Count != 0)
+            {
+                int keyBoardMaxValue = inputKeyboards.Max();
+                for (int i = 0; i < drives.Length; i++)
+                {
+                    if (keyBoardMaxValue + drives[i] <= b)
+                    {
+                        if (keyBoardMaxValue + drives[i] >= expensiveThings)
+                        {
+                            expensiveThings = keyBoardMaxValue + drives[i];
+                        }
+                    }
+                }
+
+                inputKeyboards.Remove(keyBoardMaxValue);
+            }
+
+            Console.WriteLine(expensiveThings);  // expected 9;
+        }
+    }
+}
---
+++ FindDigits/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FindDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1012;
+
+            string number = n.ToString();
+            char[] nums = number.ToCharArray();
+            int counter = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currentNumber = int.Parse(nums[i].ToString());
+                
+                if(currentNumber != 0  && n % currentNumber == 0) 
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter); // 3
+        }
+    }
+}
