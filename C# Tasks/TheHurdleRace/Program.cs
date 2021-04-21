151dba29b73126409b678e77cc970695dbd98292
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-04-21 23:31:23 +0300

    Practice

---
+++ C# Tasks/TheHurdleRace/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TheHurdleRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 4;
+            int[] height = new int[] { 1, 6, 3, 5, 2 };
+            int biggestValue = int.MinValue;
+
+            for (int i = 0; i < height.Length; i++)
+            {
+                int element = height[i];
+                if (element - k > biggestValue)
+                {
+                    biggestValue = element - k;
+                }
+            }
+
+            Console.WriteLine(biggestValue);
+        }
+    }
+}
