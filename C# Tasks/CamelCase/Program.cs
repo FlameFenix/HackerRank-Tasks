0ff74410ef237b850cd535c3af4d099a20de0e50
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-04-28 23:37:01 +0300

    Practice

---
+++ C# Tasks/CamelCase/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace CamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "saveChangesInTheEditor";
+            int counter = 1;
+            for (int i = 0; i < s.Length; i++)
+            {
+               if(char.IsUpper(s[i]))
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
