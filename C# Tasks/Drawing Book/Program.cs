807ac91b4c2676655e6cf515c6f7042b0429ba58
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-01 23:29:31 +0300

    Practice

---
+++ C# Tasks/Drawing Book/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Drawing_Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            int p = 4;
+
+            int lowestPage = int.MaxValue;
+            int countPage = 0;
+            int[] arr = new int[n];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    countPage++;
+                }
+
+                if(i == p)
+                {
+                    break;
+                }
+            }
+
+            if(countPage < lowestPage)
+            {
+                lowestPage = countPage;
+            }
+
+            countPage = 0;
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                if(i % 2 != 0)
+                {
+                    countPage++;
+                }
+
+                if (i == p)
+                {
+                    break;
+                }
+            }
+
+            if (countPage < lowestPage)
+            {
+                lowestPage = countPage;
+            }
+
+            if(arr.Length == p)
+            {
+                lowestPage = 0;
+            }
+
+            Console.WriteLine(lowestPage);
+        }
+    }
+}
