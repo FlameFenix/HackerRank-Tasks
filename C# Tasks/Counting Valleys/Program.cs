afdc411f13aa7cb80261163d520053702701ee81
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-02 19:44:23 +0300

    Practice

---
+++ C# Tasks/Counting Valleys/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace Counting_Valleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int steps = 12;
+            string path = "DDUUDDUDUUUD";
+
+            char[] stepsToCharacters = path.ToCharArray();
+
+            bool isDeep = false;
+
+            int counter = 0;
+            int countValleys = 0;
+
+            for (int i = 0; i < steps; i++)
+            {
+                char currentElement = stepsToCharacters[i];
+                if (currentElement == 'U')
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter--;
+                }
+
+                if(counter < 0)
+                {
+                    isDeep = true;
+                }
+
+                if(isDeep && counter >= 0)
+                {
+                    countValleys++;
+                    isDeep = false;
+                }
+            }
+
+            Console.WriteLine(countValleys);
+        }
+    }
+}
