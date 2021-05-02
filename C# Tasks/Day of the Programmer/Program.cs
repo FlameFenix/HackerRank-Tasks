9d76de82809ff42bf774125e0ffd343d1a2a9d4c
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-02 16:03:23 +0300

    Practice

---
+++ C# Tasks/Day of the Programmer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day_of_the_Programmer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+            int month = 9;
+            int sumDays = 0;
+
+            for (int i = 1; i <= 8; i++)
+            {
+                sumDays += DateTime.DaysInMonth(year, i);
+            }
+
+            DateTime date = new DateTime(year, month, 1);
+
+            int day = 0;
+
+            day = 256 - sumDays;
+
+            date = new DateTime(year, month, day);
+
+            if(year == 1918)
+            {
+                date = new DateTime(year, 1, 1);
+                date = date.AddDays(255 + 13);
+            }
+            // Console.WriteLine(sumDays);
+            
+             Console.WriteLine(date.ToString("dd.MM.yyyy"));
+        }
+    }
+}
