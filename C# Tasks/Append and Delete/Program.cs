902f724b834d74df76395d084b06f19ad124eb09
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-08 00:07:10 +0300

    Practice

---
+++ C# Tasks/Append and Delete/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace Append_and_Delete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 2;
+
+            string s = "ashley";
+
+            string t = "ash";
+
+            string output = string.Empty;
+
+            while (s.Length + k != t.Length && k > 0)
+            {
+                if(s.Length > 0)
+                {
+                    s = s.Remove(s.Length - 1);
+                }
+
+                k--;
+            }
+
+            while (k > 0)
+            {
+                s += t[s.Length];
+                k--;
+            }
+
+            if(s == t)
+            {
+                output = "Yes";  
+            }
+            else
+            {
+                output = "No";
+            }
+
+            Console.WriteLine(output); // expected string "ash" but is "ashl" 
+
+        }
+    }
+}
