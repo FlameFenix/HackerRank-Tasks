b74ad91e0fabbb4931ad53c8e11f43cc4a9af5f4
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-09 23:54:01 +0300

    Practice

---
+++ C# Tasks/Insertion Sort Part II/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Insertion_Sort_Part_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6] { 1, 4, 3, 5, 6, 2 };
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int j = i;
+                while (j > 0)
+                {
+                    int firstElement = arr[j - 1];
+                    int secondElement = arr[j];
+
+                    if (secondElement < firstElement)
+                    {
+                        int backup = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = backup;
+                    }
+
+                    j--;
+                }
+                Console.WriteLine(string.Join(" ", arr));
+            }
+        }
+    }
+}
