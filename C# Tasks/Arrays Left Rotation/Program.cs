d5c33c8b90b56f923a0b86cb1b073b95275d4756
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-11 00:34:50 +0300

    Practice

---
+++ C# Tasks/Arrays Left Rotation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Arrays_Left_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>() { 1, 2, 3, 4, 5 };
+            int d = 4;
+
+            for (int i = 0; i < d; i++)
+            {
+                int firstElement = a[0];
+                a.Remove(a[0]);
+                a.Insert(a.Count, firstElement);
+            }
+
+            Console.WriteLine(string.Join(" ", a));
+        }
+
+    }
+}
