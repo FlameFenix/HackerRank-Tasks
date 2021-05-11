a33a592e6e468d61f48b3355390f04882a847d62
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-12 00:02:03 +0300

    Practice

---
+++ C# Tasks/Bubble Sort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>() { 6,4,1};
+
+            int countSwaps = 0;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                for (int j = 0; j < a.Count - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int backup = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = backup;
+                        countSwaps++;
+                    }
+                }           
+            }
+            Console.WriteLine($"Array is sorted in {countSwaps} swaps.");
+            Console.WriteLine($"First Element: {a[0]}");
+            Console.WriteLine($"Last Element: {a[a.Count - 1]}");
+        }
+    }
+}
---
+++ C# Tasks/Mark and Toys/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mark_and_Toys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> prices = new List<int>() { 1, 12 ,5, 111 ,200 ,1000, 10 };
+
+            prices = prices.OrderBy(x => x).ToList();
+
+            List<int> maxLength = new List<int>();
+
+            int k = 50;
+
+            int sum = 0;
+            int index = 0;
+
+            while (sum <= k)
+            {
+                sum += prices[index];
+                if (sum <= k)
+                {
+                    maxLength.Add(prices[index]);
+                }
+                index++;
+            }
+
+            Console.WriteLine(string.Join(" ", maxLength));
+        }
+    }
+}
