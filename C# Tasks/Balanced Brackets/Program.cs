9f64c0246302cedc6d0d2425033e858ff7a04337
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-13 23:48:38 +0300

    In Progress

---
+++ C# Tasks/Balanced Brackets/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Balanced_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "{[(])}";
+
+            char[] characters = s.ToCharArray();
+
+            Stack<char> stack = new Stack<char>();
+            Queue<char> queue = new Queue<char>();
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                if(characters[i] == '{' ||
+                    characters[i] == '(' ||
+                    characters[i] == '[')
+                {
+                    stack.Push(characters[i]);
+                }
+                else
+                {
+                    queue.Enqueue(characters[i]);
+                }
+            }
+
+            bool isMatch = true;
+
+            while (stack.Count > 0 && queue.Count > 0)
+            {
+                char openBracket = stack.Pop();
+                char closedBracket = queue.Dequeue();
+                if (openBracket == '{' && closedBracket == '}' ||
+                    openBracket == '(' && closedBracket == ')' ||
+                    openBracket == '[' && closedBracket == ']')
+                {
+
+                }
+                else
+                {
+                    isMatch = false;
+                }
+            }
+
+            if(isMatch)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
