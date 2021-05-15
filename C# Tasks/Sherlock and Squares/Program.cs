a5bce1760b085951a529800b55f1ca5d529ad940
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-05-16 00:04:36 +0300

    Practice

---
+++ C# Tasks/Sherlock and Squares/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sherlock_and_Squares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 24;
+            int b = 49;
+            int x = 1;
+            int numOfSquares = 0;
+            while (x * x < a) x++;
+            while (x * x <= b)
+            {
+                numOfSquares++;
+                x++;
+            }
+            Console.WriteLine(numOfSquares);
+        }
+
+    }
+}
