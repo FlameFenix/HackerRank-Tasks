d703bc2634f64ecf2f9cfb61423c0d40649265a8
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-08-22 00:06:28 +0300

    Practice

---
+++ C# Tasks/Designer PDF Viewer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Designer_PDF_Viewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] h = new int[26] { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+
+            string word = "aba";
+
+            int tallestInteger = int.MinValue;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char currentChar = word[i];
+                int currentTall = h[(int)currentChar - 97];
+
+                if (currentTall > tallestInteger)
+                {
+                    tallestInteger = h[(int)currentChar - 97];
+                }
+
+            }
+
+            Console.WriteLine((tallestInteger * word.Length));
+        }
+    }
+}
