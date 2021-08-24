766840a9dfbbbde5ad972ccc414c6475a6e2b128
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-08-25 00:03:18 +0300

    Practice

---
+++ C# Tasks/Utopian Tree/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utopian_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int res = 0;
+            int final = 0;
+            while (res != n + 1)
+            {
+                int result = 0;
+                for (int i = 0; i <= res; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        result += 1;
+                    }
+                    else
+                    {
+                        result *= 2;
+                    }
+                }
+
+                if(final < result)
+                {
+                    final = result;
+                }
+                res++;
+            }
+
+            Console.WriteLine(final);
+        }
+    }
+}
