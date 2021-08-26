66ed298683538a833118de4092a809d7152eb75f
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-08-27 00:11:46 +0300

    8/23 failed ;(

---
+++ C# Tasks/Repeated String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Repeated_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "aba";
+            
+            long n = 100;
+
+            int containsA = s.Where(x => x == 'a').Count();
+
+            double result = Math.Ceiling((Convert.ToDouble(n) / s.Length) * containsA);
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
