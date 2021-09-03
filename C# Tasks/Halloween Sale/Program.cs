130047a5636dc4b0f6841e29bba785f8eb463caf
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-09-03 22:22:40 +0300

    Practice

---
+++ C# Tasks/Halloween Sale/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Halloween_Sale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 16; // price
+            int d = 2; // decrease price 
+            int m = 1; // minimum price
+            int s = 9981; // budget
+
+            List<int> games = new List<int>();
+
+            while (s - p >= 0)
+            {
+                s -= p;
+                games.Add(p);
+                if(p - d > m)
+                {
+                    p -= d;
+                }
+                else
+                {
+                    p = m;
+                }
+            }
+
+            Console.WriteLine(games.Count);
+        }
+    }
+}
