1ae27270a688adbe3480ed6dd258069b41224d26
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-09-30 21:24:41 +0300

    Practice

---
+++ C# Tasks/HackerRank in a String!/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HackerRank_in_a_String_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string random = "hackerrank";
+
+            string hackerrankInString = "hhaacckkekraraannk";
+
+            string isHackerRank = string.Empty;
+
+            for (int i = 0; i < hackerrankInString.Length; i++)
+            {
+                if (isHackerRank.Length < random.Length 
+                 && hackerrankInString[i] == random[isHackerRank.Length])
+                {
+                    isHackerRank += hackerrankInString[i];
+                }
+            }
+
+            string isSame = random == isHackerRank ? "YES" : "NO";
+            Console.WriteLine(isSame);
+        }
+    }
+}
