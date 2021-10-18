b12199b6312ee2938edfb64bf5a644cc69c482b4
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-10-18 23:04:16 +0300

    Practice

---
+++ C# Tasks/Beautiful Binary String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Beautiful_Binary_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string b = "0101010";
+
+            int originalStringLength = b.Length;
+
+            string a = b.Replace("010", "");
+
+            int stringAfterReplacement = a.Length;
+
+            int result = (originalStringLength - stringAfterReplacement) / 3;
+
+            Console.WriteLine(result);
+        }
+    }
+}
