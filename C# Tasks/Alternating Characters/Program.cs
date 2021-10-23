f155527557ee50fff4cad228462f4b43b8b4ee31
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-10-24 00:19:26 +0300

    Practice

---
+++ C# Tasks/Alternating Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Alternating_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "AABAAB";
+            int count = 0;
+
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                char prevChar = s[i];
+                char currentChar = s[i + 1];
+
+                if(prevChar == currentChar)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
