6251875ca149d1c789d31ea16a32925e6a529c3c
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-10-24 23:50:18 +0300

    Practice

---
+++ C# Tasks/Funny String/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Funny_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "lmnop";
+
+            int[] originalAscii = new int[s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int currentNum = s[i];
+                originalAscii[i] = currentNum;
+            }
+
+            int[] reversedAscii = originalAscii.Reverse().ToArray();
+
+            bool isFunny = true;
+
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                int firstOriginalNum = originalAscii[i];
+                int secondOriginalNum = originalAscii[i + 1];
+
+                int originalDiffrence = Math.Abs(firstOriginalNum - secondOriginalNum);
+
+                int firstReversedNum = reversedAscii[i];
+                int secondReversedNum = reversedAscii[i + 1];
+
+                int reversedDiffrence = Math.Abs(firstReversedNum - secondReversedNum);
+
+                if(originalDiffrence != reversedDiffrence)
+                {
+                    isFunny = false;
+                    break;
+                }
+            }
+            
+            if(isFunny)
+            {
+                Console.WriteLine("Funny");
+            }
+            else
+            {
+                Console.WriteLine("Not Funny");
+            }
+        }
+    }
+}
