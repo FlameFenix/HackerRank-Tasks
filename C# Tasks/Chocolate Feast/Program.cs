fe0bdca7c17f5431526d88ab6b90ee34d0fb7932
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-11-13 23:04:50 +0200

    Exercises

---
+++ C# Tasks/Chocolate Feast/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Chocolate_Feast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6; // $$
+            int c = 2; // $$ for piece
+            int m = 2; // wrapper
+
+            int countOfBars = n / c;
+            int wrappers = 0;
+            int eaten = 0;
+
+            while (countOfBars > 0)
+            {
+                eaten += countOfBars;
+                wrappers += countOfBars;
+                countOfBars = wrappers / m;
+                wrappers = wrappers % m;
+            }
+
+            Console.WriteLine(eaten);
+        }
+    }
+}
+
